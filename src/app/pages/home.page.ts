eabc045dcb6cad993f7349d6fa66b3684cc04566
Lukasz W <l.walicki1@gmail.com>
2022-03-03 20:32:46 +0100

    make better home page

--- src/app/pages/home.page.ts
+++ src/app/pages/home.page.ts
@@ -1,5 +1,18 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ViewEncapsulation,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 
-@Component({ selector: 'lib-home-page', templateUrl: './home.page.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+@Component({
+  selector: 'lib-home-page',
+  templateUrl: './home.page.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
 export class HomePage {
+  image = {
+    src: 'https://www.wojsko-polskie.pl/law/u/filer_public_thumbnails/ee/eb/eeebd332-a5c9-47b1-b316-9c1820958720/20140721094714-polska-ukraina.jpeg__1920x900_q85_crop_subsampling-2_upscale.jpg',
+    alt: 'Polska-Ukraina',
+  };
 }
