a7d7688f94c6070aa9215c833560a8b56adfc39c
Lukasz W <l.walicki1@gmail.com>
2022-03-03 20:48:11 +0100

    make better about page

--- src/app/pages/about.page.ts
+++ src/app/pages/about.page.ts
@@ -1,5 +1,34 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ViewEncapsulation,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 
-@Component({ selector: 'lib-about-page', templateUrl: './about.page.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+@Component({
+  selector: 'lib-about-page',
+  templateUrl: './about.page.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
 export class AboutPage {
+  readonly employees = {
+    first: {
+      imgSrc:
+        'https://fj-employer-blog.s3.amazonaws.com/employer-blog/wp-content/uploads/2015/11/5-Ways-to-Analyze-Employee-Performance-1024x508.jpg',
+      alt: 'first Employee',
+      description: 'Manager',
+    },
+    second: {
+      imgSrc:
+        'https://fj-employer-blog.s3.amazonaws.com/employer-blog/wp-content/uploads/2015/11/5-Ways-to-Analyze-Employee-Performance-1024x508.jpg',
+      alt: 'second Employee',
+      description: 'IT Support',
+    },
+    third: {
+      imgSrc:
+        'https://fj-employer-blog.s3.amazonaws.com/employer-blog/wp-content/uploads/2015/11/5-Ways-to-Analyze-Employee-Performance-1024x508.jpg',
+      alt: 'third Employee',
+      description: 'Developer',
+    },
+  };
 }
--- src/app/pages/home.page.ts
+++ src/app/pages/home.page.ts
@@ -11,7 +11,7 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePage {
-  image = {
+  readonly image = {
     src: 'https://www.wojsko-polskie.pl/law/u/filer_public_thumbnails/ee/eb/eeebd332-a5c9-47b1-b316-9c1820958720/20140721094714-polska-ukraina.jpeg__1920x900_q85_crop_subsampling-2_upscale.jpg',
     alt: 'Polska-Ukraina',
   };
