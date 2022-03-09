c9623d6fc83d8f86240faeb3a814fff676964199
Lukasz W <l.walicki1@gmail.com>
2022-03-09 23:46:14 +0100

    Lession 2 - excercices L2E1 - L2E5 done

--- projects/team/src/lib/adapters/primary/ui/about-us.component.ts
+++ projects/team/src/lib/adapters/primary/ui/about-us.component.ts
@@ -3,6 +3,7 @@ import {
   ViewEncapsulation,
   ChangeDetectionStrategy,
 } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'lib-about-us',
@@ -11,24 +12,24 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AboutUsComponent {
-  readonly employees = {
-    first: {
+  readonly cards$ = of([
+    {
       imgSrc:
         'https://fj-employer-blog.s3.amazonaws.com/employer-blog/wp-content/uploads/2015/11/5-Ways-to-Analyze-Employee-Performance-1024x508.jpg',
       alt: 'first Employee',
       description: 'Manager',
     },
-    second: {
+    {
       imgSrc:
         'https://fj-employer-blog.s3.amazonaws.com/employer-blog/wp-content/uploads/2015/11/5-Ways-to-Analyze-Employee-Performance-1024x508.jpg',
       alt: 'second Employee',
       description: 'IT Support',
     },
-    third: {
+    {
       imgSrc:
         'https://fj-employer-blog.s3.amazonaws.com/employer-blog/wp-content/uploads/2015/11/5-Ways-to-Analyze-Employee-Performance-1024x508.jpg',
       alt: 'third Employee',
       description: 'Developer',
     },
-  };
+  ]);
 }
--- src/app/pages/home.page-module.ts
+++ src/app/pages/home.page-module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomePage } from './home.page';
+import { OurTeamComponentModule } from '../../../projects/team/src/lib/adapters/primary/ui/our-team.component-module';
+import { AboutUsComponentModule } from '../../../projects/team/src/lib/adapters/primary/ui/about-us.component-module';
 
-@NgModule({ imports: [CommonModule, 
-      RouterModule.forChild([
-        {
-          path: '',
-          component: HomePage,
-        }
-      ])],
-  	declarations: [HomePage],
-  	providers: [],
-  	exports: [] })
-export class HomePageModule {
-}
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage,
+      },
+    ]),
+    OurTeamComponentModule,
+    AboutUsComponentModule
+  ],
+  declarations: [HomePage],
+  providers: [],
+  exports: [],
+})
+export class HomePageModule {}
