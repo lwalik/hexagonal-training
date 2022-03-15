d29b8d9b3ac7534c185dbd59f6ea9a09d5591a8b
Lukasz W <l.walicki1@gmail.com>
2022-03-15 23:52:16 +0100

    use EmployeeCardComponent in EmployeesComponent and EmployeeDetailsComponent

--- projects/team/src/lib/adapters/primary/ui/employee-details.component-module.ts
+++ projects/team/src/lib/adapters/primary/ui/employee-details.component-module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmployeeDetailsComponent } from './employee-details.component';
 import { RouterModule } from '@angular/router';
+import { EmployeeCardComponentModule } from './employee-card.component-module';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, EmployeeCardComponentModule],
   declarations: [EmployeeDetailsComponent],
   providers: [],
   exports: [EmployeeDetailsComponent],
--- projects/team/src/lib/adapters/primary/ui/employees.component-module.ts
+++ projects/team/src/lib/adapters/primary/ui/employees.component-module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { EmployeesComponent } from './employees.component';
+import { EmployeeCardComponentModule } from './employee-card.component-module';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, EmployeeCardComponentModule],
   declarations: [EmployeesComponent],
   providers: [],
   exports: [EmployeesComponent],
