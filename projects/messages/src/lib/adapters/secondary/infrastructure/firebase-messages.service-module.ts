d639c5736cf27bc86f51b2fd546121ef78bcf57f
Lukasz W <l.walicki1@gmail.com>
2022-03-15 18:27:31 +0100

    implement Add message dto port

--- projects/messages/src/lib/adapters/secondary/infrastructure/firebase-messages.service-module.ts
+++ projects/messages/src/lib/adapters/secondary/infrastructure/firebase-messages.service-module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { FirebaseMessagesService } from './firebase-messages.service';
+import { ADDS_MESSAGE_DTO } from '../../../application/ports/secondary/adds-message.dto-port';
 
 @NgModule({ imports: [AngularFirestoreModule],
   	declarations: [],
-  	providers: [FirebaseMessagesService],
+  	providers: [FirebaseMessagesService, { provide: ADDS_MESSAGE_DTO, useExisting: FirebaseMessagesService }],
   	exports: [] })
 export class FirebaseMessagesServiceModule {
 }
--- projects/messages/src/lib/adapters/secondary/infrastructure/firebase-messages.service.ts
+++ projects/messages/src/lib/adapters/secondary/infrastructure/firebase-messages.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { AddsMessageDtoPort } from '../../../application/ports/secondary/adds-message.dto-port';
+import { MessageDTO } from '../../../application/ports/secondary/message.dto';
 
 @Injectable()
-export class FirebaseMessagesService {
-  constructor(private _client: AngularFirestore) {
+export class FirebaseMessagesService implements AddsMessageDtoPort {
+  constructor(private _client: AngularFirestore) {}
+
+  add(message: Partial<MessageDTO>): void {
+    this._client.collection('messages').add(message);
   }
 }
