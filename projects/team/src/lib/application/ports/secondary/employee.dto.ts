3625a24c200e1db13309765eca37d31b902ae04e
Lukasz W <l.walicki1@gmail.com>
2022-03-16 00:26:58 +0100

    add id in EmployeeDTO

--- projects/team/src/lib/application/ports/secondary/employee.dto.ts
+++ projects/team/src/lib/application/ports/secondary/employee.dto.ts
@@ -2,6 +2,7 @@ import { CountryDTO } from './country.dto';
 import { DepartmentDTO } from './department.dto';
 
 export interface EmployeeDTO {
+  id: string;
   readonly bio: string;
   readonly age: number;
   readonly country: CountryDTO;
