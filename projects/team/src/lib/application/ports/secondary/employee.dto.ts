c7e020119ed465de6cf4404029c9589f90ef6ddc
Lukasz W <l.walicki1@gmail.com>
2022-03-17 23:31:34 +0100

    add imageURL in EmployeeDTO

--- projects/team/src/lib/application/ports/secondary/employee.dto.ts
+++ projects/team/src/lib/application/ports/secondary/employee.dto.ts
@@ -5,6 +5,7 @@ export interface EmployeeDTO {
   readonly id: string;
   readonly bio: string;
   readonly age: number;
+  readonly imageURL: string;
   readonly country: CountryDTO;
   readonly department: DepartmentDTO;
 }
