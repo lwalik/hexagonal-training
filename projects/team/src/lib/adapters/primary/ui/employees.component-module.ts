928626f58cfb85ed716547355b38309ba44810d7
Lukasz W <l.walicki1@gmail.com>
2022-03-18 00:57:29 +0100

    use employeeDetailsComponent in employeePage

--- projects/team/src/lib/adapters/primary/ui/employees.component-module.ts
+++ projects/team/src/lib/adapters/primary/ui/employees.component-module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { EmployeesComponent } from './employees.component';
+import { EmployeeDetailsComponentModule } from './employee-details.component-module';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, EmployeeDetailsComponentModule],
   declarations: [EmployeesComponent],
   providers: [],
   exports: [EmployeesComponent],
